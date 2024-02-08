138f0433fbabe721cc8fdbbbafef2425766f4add
Sathuskumar <140132883+SK0511@users.noreply.github.com>
2024-02-08 13:26:20 +0530

    Update ex5.sh

--- ex5.sh
+++ ex5.sh
@@ -1,9 +1,8 @@
 #! /bin/bash
 
-#SIMPLE ARRAY
-NAME[0]="ROKY"
-NAME[1]="JOHN"
-NAME[2]="WILLIUM"
-NAME[3]="ALEX"
+NAME[0]="SATHIS"
+NAME[1]="KUMAR"
+NAME[2]="VARUN"
+NAME[3]="SOBEN"
 echo "First Index: ${NAME[0]}"
 echo "Second Index: ${NAME[1]}"
