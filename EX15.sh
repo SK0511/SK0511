5d13164e587026dacb5ec4ad7ff7f8473d915344
Sathuskumar <140132883+SK0511@users.noreply.github.com>
2024-02-08 15:51:11 +0530

    Create EX15.sh

---
+++ EX15.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+
+org="Nia technologies"
+netid="198.623.24.00"
+sc="30"
+int="active"
+
+
+
+echo "your oragization name is $org"
+echo "your network id is $netid"
+echo "your sever count is $sc"
+echo "your internet status is $int"
+
+
+cho "thanking for viewing the details"
